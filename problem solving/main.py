1f2d1a477f3fa494af341ecf112bfaa47124c841
Dhanush Kumar M <dhanushmadan404@gmail.com>
2026-01-17 10:15:26 +0530

    got it

---
+++ problem solving/main.py
@@ -0,0 +1,14 @@
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+common=[]
+possibility1={nums[0]:nums[0]}
+ans=n(iter(possibility1.values()))
+print(ans)
+# possibility2=nums[0] 
+
+# for i in range(len(nums)):
+#     common=[]
+#     for y in range(len(nums)):
+#         common.append(nums[y])
+#         if sum(common)>possibility1.values()[0]:
+#             possibility1={common:sum(common)}
+# print(possibility1)
---
+++ problem solving/script.js
@@ -0,0 +1,15 @@
+// Problem:
+// Find the largest sum of any subarray.
+
+// Example
+
+let nums = [-2,1,-3,4,-1,2,1,-5,4]
+let common=[]
+let possibility1=nums[0]
+let possibility2=null 
+
+for(let i=0;i<=nums.length();i++){
+    for(let y=0;y<=nums.length();y++){
+        possibility1
+    }
+}
